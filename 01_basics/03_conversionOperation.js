85835c8ed0e83e3ac205af06bc1032c7ccf0444d
cryptojav <hammasdevjaved@gmail.com>
2026-07-13 12:05:44 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+//let score = null
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let ValueInNumber = Number(score)
+
+//console.log(typeof Number)
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber)
+
+//"33" => Number
+//"33abc" => NaN
+// true = 1; False = 0
+
+//let IsLoggedIn = undefined
+
+//BooleanIsLoggedIn = Boolean(IsLoggedIn)
+//console.log(BooleanIsLoggedIn)
+
+//1 => true 
+//0 => false
+//"" => false
+//"hammas"=> true
+
+let someNumber = 22
+
+let stringNumber = string(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
