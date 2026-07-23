45330713992347a40f02f527125a73e105a64d65
Hammas Javed <hammasdevjaved@gmail.com>
2026-07-23 11:30:17 +0000

    Arrays practice

---
+++ 02_basics/arrays.js
@@ -0,0 +1,163 @@
+//syntax
+
+//const myArr = ["a", 1, 2, 3, "hammas"]
+
+//console.log(myArr.at(-2))
+
+// Q. Create an array containing your name, age, and city
+
+/*const myArr = ["Hammas", 21, "Islamabad"]
+console.log(myArr[0])
+console.log(myArr[1])
+console.log(myArr[2])
+
+const myArr = ["Hammas", 21, "Islamabad"]
+console.log(...myArr)
+
+const myArr = [1, 2, 3, 5, 7, 8, 15]
+   //print the length of array
+console.log(myArr.length)
+
+const myArr = ["Hammas", 21, "Islamabad"]
+// replace the second element with another value
+myArr[0] = 20
+console.log(myArr)
+
+const myArr = ["Hammas", 21, "Islamabad"]
+//adding one more element at the end
+myArr.pop("Islamabad")
+console.log(myArr)
+
+const myArr = ["Hammas", 21, "Islamabad"]
+//adding an element at the beggning
+myArr.unshift("Mr .") 
+console.log(myArr)
+
+const myArr = ["Hammas", 21, "Islamabad"]
+if (myArr == 0)
+{
+    console.log("array is empty")
+}
+else{
+    console.log("array is not empty")
+}
+
+const person = ["mango", "bannana", "Grapes", "Apple"]
+//printing array using for loop()
+for(let i = 0; i < person.length; i++)
+{
+    console.log(person[i])
+}
+
+const person = ["Ali", "Ahmad", "Hammas", "Hamza"]
+for(element of person){
+    console.log(element)
+}
+
+const person = ["Ali", "Ahmad", "Hammas", "Hamza"]
+//print every element with index
+for(let i = 0; i < person.length; i++)
+{
+    console.log(`Index: ${i}, Value: ${person[i]}`)
+}
+
+const fruits = ["Mango", "Apple", "Grapes", "Bannana", "peach"]
+//find the middle element
+const midIndex = Math.floor(fruits.length / 2)
+console.log(fruits[midIndex])
+
+const fruits = ["Mango", "Apple", "Grapes", "Bannana", "peach"]
+//swapping first element with last element
+const firstFruit = fruits[0]
+[fruits[0], fruits[fruits.length - 1]] = [fruits[fruits.length - 1] , fruits[0]]
+console.log(fruits)
+
+const fruits = ["Mango", "Apple", "Grapes", "Bannana", "peach"]
+//writing a for loop to print the array in reverse order
+for( let i = fruits.length -1; i >=0; i--)
+{
+    console.log(fruits[i])
+}
+
+const numbers = [ 12, 13, 14, 10, 4, 6, 5, 8]
+for( let i = 0; i < numbers.length; i++)
+{
+    if(numbers[i] %2 == 0)
+    {
+        console.log(numbers[i])
+    }
+}
+let sum = 0
+const numbers = [ 12, 13, 14, 10, 4, 6, 5, 8]
+for(let i = 0; i < numbers.length; i++)
+{
+    sum += numbers[i]
+}
+console.log("Sum: ",sum)
+let sum = 0
+let avg = 0
+const numbers = [ 12, 13, 14, 10, 4, 6, 5, 8]
+for(let i = 0; i < numbers.length; i++)
+{
+
+    sum+= numbers[i]
+    let total = numbers.length
+    avg = sum/total
+
+}
+console.log("Avg: ", avg)
+
+
+
+const numbers = [ 12, 13, 14, 10, 45, 46, 35, 8]
+
+let largest = numbers[0]
+
+for(let number of numbers)
+{
+    if(number>largest)
+    {
+        largest = number
+    }
+}
+console.log("largest: ", largest)
+
+const numbers = [ 12, 13, 14, 10, 45, 46, 35, 8]
+
+let smallest = numbers[0]
+for(let number of numbers)
+{
+    if(number < smallest)
+    {
+        smallest = number
+    }
+}
+console.log("smallest: ",smallest)
+
+
+const numbers = [ 12, 13, 1, -8, -4, 6, 20, -7]
+
+let count = 0
+for(let number of numbers)
+{
+    if(number>0)
+    {
+        count++
+    }
+}
+console.log("Positive Numbers: ",count)
+
+const numbers = [ 12, 13, 1, -8, -4, 6, 20, -7]
+//counting negative numbers
+let count = 0
+for(let number of numbers)
+{
+    if(number<0)
+    {
+        count++
+    }
+}
+console.log("Positive Numbers: ",count)*/
+
+
+
